83089b89077ae312b0accee044d8fb6ea24e4bb5
unknown <luofei02@99F82EFB3D123D7.internal.baidu.com>
2015-02-11 15:23:17 +0800

    update

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 ﻿var config = {
 	/*每帧数据的宽度*/
-	width: 36,
+	width: 308,
 	/*每帧数据的高度*/
-	height: 36,
+	height: 227,
 	/*强烈建议设置为0即可！不然或许会引起不必要的麻烦*/
 	gap: 0,
 	/*共有多少张*/
-	frames: 7,
+	frames: 6,
 	/*待压缩的文件目录*/
 	imgSrc: './img/',
 	/*输出目录，如果为空则在根目录输出*/
 	dest: './tmp/',
 	/*拼合图片的背景是否透明*/
-	transparent: false,
+	transparent: true,
 	/*是否严格验证给定的width，height拼合；否则按照图片原有尺寸进行拼合*/
 	isStrict: false,
 	/*输出文件名*/
